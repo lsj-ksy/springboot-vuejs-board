08dbd0af1b730114cac3ac7fee33f0c223f9c3e2
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-08-03 04:43:05 +0900

    templates demo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import mixins from "../blob/mixins";
+import mixins from "../blob/mixins"
+
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const app = createApp(App)
 app.use(router)
