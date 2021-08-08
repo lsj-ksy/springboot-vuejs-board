33ff95df5b16842d45ecae5e3538e0614dc08b69
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-08-09 06:29:45 +0900

    [New] Main api data

---
+++ src/main/java/com/sjsy/springvue/web/dto/MainFileResDto.java
@@ -0,0 +1,19 @@
+package com.sjsy.springvue.web.dto;
+
+import com.sjsy.springvue.domain.board.Post;
+import com.sjsy.springvue.domain.main.ContentFile;
+import lombok.Getter;
+import lombok.ToString;
+
+//대문 전체글보기 ResponseDto
+@Getter
+@ToString
+public class MainFileResDto {
+
+    private String fileName;
+
+    public MainFileResDto(ContentFile entity) {
+        this.fileName = entity.getFileName();
+    }
+
+}
