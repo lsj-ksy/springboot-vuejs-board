223cf0d8d3fad2b7a418553c600f05a57976d55f
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-09-18 02:30:12 +0900

    [Add] WebConfig, Reply modify

--- src/main/java/com/sjsy/springvue/web/dto/request/ReplyUpdateReqDto.java
+++ src/main/java/com/sjsy/springvue/web/dto/request/ReplyUpdateReqDto.java
@@ -2,8 +2,10 @@ package com.sjsy.springvue.web.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class ReplyUpdateReqDto {
 
     private String userId;
